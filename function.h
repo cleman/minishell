6cb3f23279b32381b2f91fe685c826e28badaa2d
clement <Clement.lebreton@etu.sorbonne-universite.fr>
2023-11-14 10:41:33 +0100

    .

---
+++ function.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION
+#define FUNCTION
+
+
+
+#endif
--- minishell.c
+++ minishell.c
@@ -9,42 +9,67 @@
 
 int main(int argc, char **argv) {
     char *argv_execv[10];
+    argv_execv[0] = "";
 
     char entry[50];
 
     char *strToken;
     int i;
+    int test;
 
-    while (strcmp(entry, "exit\0") != 0) {
-        // Affichage de l'invite de command
+    while (strcmp(argv_execv[0], "exit\0") != 0) {
+        // Affichage de l'invite de commande
         printf("$ ");
         fgets(entry, 50, stdin);
 
-        // Supprimer le saut de ligne à la fin de l'entrée
+        // Modification caractère de fin
         size_t len = strlen(entry);
-
-        if (len > 0 && entry[len - 1] == '\n') {
-            entry[len - 1] = '\0';
-        }
-
-        strToken = strtok(entry, " ");
-        argv_execv[0] = strToken;
-        i = 1;
-        while (strToken != NULL) {
-            strToken = strtok(NULL, " ");
-            argv_execv[i] = strToken;
-            i++;
-        }
-        argv_execv[i] = NULL;
-
-        if (strcmp(entry,"exit\0") != 0) {
-
-            if (fork() == 0) {
-                if (execvp(argv_execv[0], argv_execv) == -1) perror("Erreur lors du execv");
-                exit(0);
+        if (len > 1) {
+            if (len > 0 && entry[len - 1] == '\n') {
+                entry[len - 1] = '\0';
             }
         }
-        wait(NULL);
+
+        test = 0;
+        for (int n = 0; n < len-1; n++) {
+            if (entry[n] != ' ') {
+                test = 1;
+                break;
+            }
+        }
+
+        if (test == 1) {
+            // Découpage avec " "
+            strToken = strtok(entry, " ");
+            argv_execv[0] = strToken;
+            i = 1;
+            while (strToken != NULL) {
+                strToken = strtok(NULL, " ");
+                argv_execv[i] = strToken;
+                i++;
+            }
+            //if (i == 2) len = 1;
+            argv_execv[i] = NULL;
+
+            if (strcmp(argv_execv[0],"exit\0") != 0 && len > 1) {
+
+                if (strcmp(argv_execv[0],"cd") != 0 && strcmp(argv_execv[0],"pwd") != 0) {
+                    if (fork() == 0) {
+                        if (execvp(argv_execv[0], argv_execv) == -1) perror("Erreur lors du execv");
+                        exit(0);
+                    }
+                    wait(NULL);
+                }
+                else if (strcmp(argv_execv[0],"cd") == 0) {
+                    chdir(argv_execv[1]);
+                }
+                else if (strcmp(argv_execv[0],"pwd") == 0) {
+                    char buff[200];
+                    getcwd(buff, sizeof buff);
+                    printf("%s\n", buff);
+                }
+            }
+        }
     }
 
     return 0;
