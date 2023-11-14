967e8897a5bc17090f80a44c6d7d4c003d695016
clement <Clement.lebreton@etu.sorbonne-universite.fr>
2023-11-14 07:37:16 +0100

    new ls, who sans fork

--- Makefile
+++ Makefile
@@ -6,6 +6,11 @@ all : $(EXEC)
 minishell: minishell.c
 	gcc $(CFLAGS) -o minishell minishell.c
 
+git:
+	git add .
+	git commit -m ""
+	git push
+
 clean:
 	rm -f *.o *~ *.backup
 
--- minishell.c
+++ minishell.c
@@ -1,17 +1,36 @@
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
-    if (argc != 2) return 0;
+    //if (argc != 2) return 0;
 
     char path[50];
     char *argv_execv[2];
     argv_execv[1] = NULL;
 
-    if (strcmp(argv[1],"ls") || strcmp(argv[1],"who")) {
-        argv_execv[0] = argv[1];
-        strcpy(path,"/bin/");
-        strcat(path, argv[1]);
-        execv(path, argv_execv);
+    printf("On est là\n");
+    char entry[50];
+
+    while (strcmp(entry, "exit\n") != 1) {
+        printf("$ ");
+        fgets(entry, 50, stdin);
+
+        // Supprimer le saut de ligne à la fin de l'entrée
+        size_t len = strlen(entry);
+
+        if (len > 0 && entry[len - 1] == '\n') {
+            entry[len - 1] = '\0';
+        }
+
+        if (strcmp(entry,"ls") || strcmp(entry,"who")) {
+            argv_execv[0] = entry;
+            strcpy(path,"/bin/");
+            strcat(path, entry);
+
+            if (execv(path, argv_execv) == -1) perror("Erreur lors du execv");
+        }
     }
+
+    return 0;
 }
